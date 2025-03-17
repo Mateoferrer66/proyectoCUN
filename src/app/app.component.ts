634b0b888573d2e7c70f62a80171387fc40f8555
Mateo Ferrer <mateoferrer66@gmail.com>
2025-03-17 17:26:41 -0500

    v0.3 CUN

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,16 @@ export class AppComponent {
   imagen2: string = 'assets/Frame 200.png';  // Ruta de la imagen corregida
   imagen3: string = 'assets/buscarymenu.png';  // Ruta de la imagen corregida
   imagen4: string = 'assets/iconohome.png';  // Ruta de la imagen corregida
+  imagen5: string = 'assets/iconocivil1.png';
+  imagen6: string = 'assets/iconocivil2.png';
+  imagen7: string = 'assets/iconocivil3.png';
+  imagen8: string = 'assets/iconocivil4.png';  // Ruta de la imagen corregida 
+  imagen9: string = 'assets/iconofacebook.png';  // Ruta de la imagen corregida
+  imagen10: string = 'assets/iconotiktok.png';  // Ruta de la imagen corregida
+  imagen11: string = 'assets/iconoinsta.png';  // Ruta de la imagen corregida
+  imagen12: string = 'assets/iconoyoutube.png';  // Ruta de la imagen corregida
+  imagen13: string = 'assets/iconolinkedin.png';  // Ruta de la imagen corregida
+  imagen14: string = 'assets/iconowhatsapp.png';  // Ruta de la imagen corregida
+  imagen15: string = 'assets/iconotel.png';  // Ruta de la imagen corregida
+  imagen16: string = 'assets/iconocorreo.png';  // Ruta de la imagen corregida
 }
